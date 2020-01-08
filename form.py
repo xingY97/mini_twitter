723aa8cc2c332e0ac3c757fb9d8ca777e04c4a42
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-01-07 21:50:15 -0800

    log in form created

---
+++ form.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+form stforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+     username = StringField('Username', validators=[DataRequired(), Length(min=5,max=20)]) 
+
+     email = StringField('Email',validators=[DataRequired(), Email()])
+
+     password = PasswordField('Password', validators=[DataRequired()])
+
+     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+     submit = SubmitField('Sign Up')
